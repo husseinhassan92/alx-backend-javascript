d9443b44d7105f1415a1050e3a16e7b979339e4b
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-11-27 19:30:30 +0200

    task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(...theArgs) {
+    const budget = {
+      income: arguments[0],
+      gdp: arguments[1],
+      capita: arguments[2],
+    };
+  
+    return budget;
+  }
