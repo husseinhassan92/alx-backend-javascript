f031090e2c84af16a6efda81862584f4b15ce548
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-11-28 20:51:05 +0200

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,5 +5,6 @@ export default function handleResponseFromAPI(promise) {
       body: 'success',
     }))
     .catch(() => Error())
+    // eslint-disable-next-line no-console
     .finally(() => console.log('Got a response from the API'));
 }
